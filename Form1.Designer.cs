7f478b8369805e2d54a33738db6765c2c7cb065f
LoOny <loonybiker@yandex.ru>
2015-07-22 00:10:25 +0300

    One thread version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 53);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(371, 43);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(33, 115);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(104, 33);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(260, 115);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(104, 33);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(370, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "So... Yeah, it\'s just a simple form doing absolutely nothing. Just simulating som" +
+    "e activity. You could start simulation or stop it";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 171);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,45 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace WinFormThreads
 {
     public partial class Form1 : Form
     {
+        private WorkSimulator sim;
+
         public Form1()
         {
             InitializeComponent();
+
+            btnStart.Click += btnStart_Click;
+            btnStop.Click += btnStop_Click;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            sim = new WorkSimulator();
+            sim.progressChanged += progressChanged;
+            sim.isWorkComplite += simulationCompleted;
+
+            btnStart.Enabled = false;
+            sim.Work();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simulationCompleted(bool cancelled)
+        {
+            string msg = cancelled ? "Process cancelled." : "Progress completed";
+            MessageBox.Show(msg);
+            btnStart.Enabled = true;
+        }
+
+        private void progressChanged(int progressValue)
+        {
+            progressBar1.Value = progressValue;
         }
     }
 }
