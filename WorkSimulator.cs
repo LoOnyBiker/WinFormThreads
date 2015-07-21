7a976c8a1d052eedc2ce274cf7a107ccf7f901f8
LoOny <loonybiker@yandex.ru>
2015-07-21 23:42:41 +0300

    Delete unnecessary links, add WorkSimulator class

---
+++ WorkSimulator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace WinFormThreads
+{
+    public sealed class WorkSimulator
+    {
+
+        private bool _cancelled = false;
+
+        public void Work()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (_cancelled)
+                    break;
+                Thread.Sleep(100);
+                progressChanged(i);
+            }
+            isWorkComplite(_cancelled);
+        }
+
+        public void Cancel()
+        {
+            _cancelled = true;
+        }
+
+        public event Action<int> progressChanged;
+        public event Action<bool> isWorkComplite;
+
+    }
+}
